9d45e03a67c3608855aa1c4576fdc51643c6c56d
ASTIC <github@cl-mail.eu>
2021-07-03 14:42:12 +0200

    Ordnerstruktur angepasst und Testprojekt hinzugefügt

---
+++ TranslateCSVTests/ProtectSpecialsTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace TranslateCSV.Tests
+{
+    [TestClass]
+    public class ProtectSpecialsTests
+    {
+        [TestMethod]
+        public void ProtectTestNone()
+        {
+            var p = new ProtectSpecials()
+            {
+                ProtectWords = new[] { new Regex(@"(?'protect'\[(\S+?)\])") },
+                Glossar      = new Dictionary<Regex, string> { { new Regex(@"(?'replace'report)[^\S]"), "report"} }
+            };
+
+            var text = "[c][FFFF00]Want to report a game bug?[-][/c]\\nPlease reproduce it in a unmodded, default game before reporting it to the Empyrion developers.";
+
+            Assert.AreEqual(text, p.Restore(p.Protect(text)));
+        }
+    }
+}
